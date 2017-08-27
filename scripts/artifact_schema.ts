c6e02387ddb2499b6f653b669ddfff915f491791
Amir Bandeali <abandeali1@gmail.com>
2017-08-26 23:00:46 -0700

    Add optimizer_runs to ContractData and check before recompiling

--- scripts/artifact_schema.ts
+++ scripts/artifact_schema.ts
@@ -9,6 +9,7 @@ export interface ContractNetworks {
 
 export interface ContractData {
     solc_version: string;
+    optimizer_runs: number;
     keccak256: string;
     abi: any[];
     unlinked_binary: string;
