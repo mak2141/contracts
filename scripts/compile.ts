038478d8f158d01b1c9aa3df81c221f8c571e08e
Amir Bandeali <abandeali1@gmail.com>
2017-08-26 22:29:02 -0700

    Add keccak256 to ContractData schema, do not recompile if source keccak256 already exists

--- scripts/compile.ts
+++ scripts/compile.ts
@@ -5,8 +5,9 @@ import * as promisify from 'es6-promisify';
 import * as path from 'path';
 import * as _ from 'lodash';
 import * as semver from 'semver';
+import ethUtil =  require('ethereumjs-util');
 import {binPaths} from './solc/bin_paths';
-import {SolcOutput, ContractData} from './schema';
+import {ContractArtifact, ContractData} from './artifact_schema';
 
 const DEFAULT_OPTIMIZER_RUNS = 0;
 const DEFAULT_CONTRACTS_DIR = path.resolve('contracts');
@@ -46,42 +47,55 @@ class Compiler {
         }
         const contractBaseNames = _.keys(sources);
         _.each(contractBaseNames, async contractBaseName => {
-            const input = {[contractBaseName]: sources[contractBaseName]};
-            const solcVersion = this.parseSolidityVersion(input[contractBaseName]);
-            const fullSolcVersion = binPaths[solcVersion];
-            const solcBinPath = `./solc/solc_bin/${fullSolcVersion}`;
-            const solcBin = require(solcBinPath);
-            const solcInstance = solc.setupMethods(solcBin);
-
-            console.log(`Compiling ${contractBaseName}...`);
-            const compiled = solcInstance.compile({sources: input}, this.optimizerRuns, findImports);
-
+            const source = sources[contractBaseName];
             const contractName = path.basename(contractBaseName, '.sol');
-            const contractIdentifier = `${contractBaseName}:${contractName}`;
-            const contractData: ContractData = {
-                solc_version: solcVersion,
-                abi: JSON.parse(compiled.contracts[contractIdentifier].interface),
-                unlinked_binary: `0x${compiled.contracts[contractIdentifier].bytecode}`,
-            };
-
             const currentArtifactPath = `${path.resolve('build')}/artifacts/${contractName}.json`;
-            let newArtifact: SolcOutput;
+            const sourceHash = `0x${ethUtil.sha3(source).toString('hex')}`;
+
+            let currentArtifactString;
+            let currentArtifact: ContractArtifact;
+            let oldNetworks;
+            let shouldCompile = true;
             try {
-                const currentArtifactString = await readFileAsync(currentArtifactPath, {encoding: 'utf8'});
-                const currentArtifact = JSON.parse(currentArtifactString);
-                const oldNetworks = currentArtifact.networks;
-                if (!_.isEqual(oldNetworks[this.networkId], contractData)) {
+                currentArtifactString = await readFileAsync(currentArtifactPath, {encoding: 'utf8'});
+                currentArtifact = JSON.parse(currentArtifactString);
+                oldNetworks = currentArtifact.networks;
+                const oldNetwork: ContractData = oldNetworks[this.networkId];
+                if (oldNetwork && oldNetwork.keccak256 === sourceHash) {
+                    shouldCompile = false;
+                }
+            } catch (err) {} // should always compile if file does not exist
+
+            if (shouldCompile) {
+                const input = {[contractBaseName]: source};
+                const solcVersion = this.parseSolidityVersion(input[contractBaseName]);
+                const fullSolcVersion = binPaths[solcVersion];
+                const solcBinPath = `./solc/solc_bin/${fullSolcVersion}`;
+                const solcBin = require(solcBinPath);
+                const solcInstance = solc.setupMethods(solcBin);
+
+                console.log(`Compiling ${contractBaseName}...`);
+                const compiled = solcInstance.compile({sources: input}, this.optimizerRuns, findImports);
+
+                const contractIdentifier = `${contractBaseName}:${contractName}`;
+                const contractData: ContractData = {
+                    solc_version: solcVersion,
+                    keccak256: sourceHash,
+                    abi: JSON.parse(compiled.contracts[contractIdentifier].interface),
+                    unlinked_binary: `0x${compiled.contracts[contractIdentifier].bytecode}`,
+                };
+
+                let newArtifact: ContractArtifact;
+                if (!_.isUndefined(currentArtifactString)) {
                     const newNetworks = _.assign({}, oldNetworks, {[this.networkId]: contractData});
                     newArtifact = _.assign({}, currentArtifact, {networks: newNetworks});
+                } else {
+                    newArtifact = {
+                        contract_name: contractName,
+                        networks: {[this.networkId]: contractData},
+                    };
                 }
-            } catch (err) {
-                newArtifact = {
-                    contract_name: contractName,
-                    networks: {[this.networkId]: contractData},
-                };
-            }
 
-            if (!_.isUndefined(newArtifact)) {
                 await writeFileAsync(currentArtifactPath, JSON.stringify(newArtifact));
                 console.log(`${contractBaseName} artifact saved!`)
             }
