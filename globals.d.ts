d1721928ac78ca33e068c7e88ae2f02e2421d014
Amir Bandeali <abandeali1@gmail.com>
2017-09-07 15:14:12 -0700

    Add types, declare modules

--- globals.d.ts
+++ globals.d.ts
@@ -1,8 +1,8 @@
 declare module 'bn.js';
 declare module 'ethereumjs-abi';
-declare module 'es6-promisify';
 declare module 'chai-bignumber';
 declare module 'dirty-chai';
+declare module 'yargs';
 
 // HACK: In order to merge the bignumber declaration added by chai-bignumber to the chai Assertion
 // interface we must use `namespace` as the Chai definitelyTyped definition does. Since we otherwise
@@ -16,14 +16,26 @@ declare namespace Chai {
     }
 }
 /* tslint:enable */
-declare module 'yargs';
-declare module 'solc';
 
 declare module '*.json' {
     const json: any;
     /* tslint:disable */
     export default json;
     /* tslint:enable */
+}
+
+declare module 'solc' {
+    function compile(sources: any, optimizerEnabled: number, findImports: (importPath: string) => any): any;
+    function setupMethods(solcBin: any): any;
+    export = {
+        compile,
+        setupMethods,
+    };
+}
+
+declare module 'es6-promisify' {
+    function promisify(original: any, settings?: any): ((...arg: any[]) => Promise<any>);
+    export = promisify;
 }
 
 // Truffle injects the following into the global scope
--- util/types.ts
+++ util/types.ts
@@ -117,3 +117,41 @@ export enum ExchangeContractErrs {
   ERROR_ROUNDING_ERROR_TOO_LARGE,
   ERROR_INSUFFICIENT_BALANCE_OR_ALLOWANCE,
 }
+
+export interface ContractArtifact {
+    contract_name: string;
+    networks: ContractNetworks;
+};
+
+export interface ContractNetworks {
+    [key: number]: ContractData;
+};
+
+export interface ContractData {
+    solc_version: string;
+    optimizer_runs: number;
+    keccak256: string;
+    abi: Web3.ContractAbi;
+    unlinked_binary: string;
+    address?: string;
+    updated_at: number;
+};
+
+export interface SolcErrors {
+    [key: string]: boolean;
+};
+
+export interface CompilerOptions {
+    contractsDir: string;
+    networkId: number;
+    optimizerRuns: number;
+    artifactsDir: string;
+};
+
+export interface ContractSources {
+    [key: string]: string;
+};
+
+export interface ImportContents {
+    contents: string;
+};
