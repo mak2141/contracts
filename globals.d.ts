ec5e023a7b31d8aa7351461da39e65c378d423e0
Amir Bandeali <abandeali1@gmail.com>
2017-09-07 15:14:12 -0700

    Add types, declare modules

--- globals.d.ts
+++ globals.d.ts
@@ -1,8 +1,5 @@
 declare module 'bn.js';
 declare module 'ethereumjs-abi';
-declare module 'es6-promisify';
-declare module 'yargs';
-declare module 'solc';
 
 declare module '*.json' {
     const json: any;
@@ -20,30 +17,16 @@ declare var beforeEach: any;
 declare var describe: any;
 declare var it: any;
 
-declare module 'ethereumjs-util' {
-  function bufferToHex(value: Buffer): string;
-  function ecrecover(msgHash: Buffer, v: number, r: Buffer, s: Buffer): Buffer;
-  function fromRpcSig(sig: string): {v: number, r: Buffer, s: Buffer};
-  function hashPersonalMessage(hash: Buffer): Buffer;
-  function isHexString(value: any): boolean;
-  function pubToAddress(pubKey: Buffer, sanitize?: boolean): Buffer;
-  function setLength(a: Buffer, length: number): Buffer;
-  function setLengthLeft(a: Buffer, length: number): Buffer;
-  function sha3(a: Buffer|string|number, bits?: number): Buffer;
-  function toBuffer(value: any): Buffer;
-  function isValidAddress(address: string): boolean;
+declare module 'solc' {
+    function compile(sources: any, optimizerEnabled: number, findImports: (importPath: string) => any): any;
+    function setupMethods(solcBin: any): any;
+    export = {
+        compile,
+        setupMethods,
+    };
+}
 
-  export = {
-            bufferToHex,
-            ecrecover,
-            fromRpcSig,
-            hashPersonalMessage,
-            isHexString,
-            pubToAddress,
-            setLength,
-            setLengthLeft,
-            sha3,
-            toBuffer,
-            isValidAddress,
-          };
+declare module 'es6-promisify' {
+    function promisify(original: any, settings?: any): ((...arg: any[]) => Promise<any>);
+    export = promisify;
 }
--- util/types.ts
+++ util/types.ts
@@ -1,4 +1,5 @@
 import * as BigNumber from 'bignumber.js';
+import * as Web3 from 'web3';
 
 export interface BalancesByOwner {
   [ownerAddress: string]: {
@@ -131,3 +132,41 @@ export enum ExchangeContractErrs {
   ERROR_ROUNDING_ERROR_TOO_LARGE,
   ERROR_INSUFFICIENT_BALANCE_OR_ALLOWANCE,
 }
+
+export interface ContractArtifact {
+    contract_name: string;
+    networks: ContractNetworks;
+};
+
+export interface ContractNetworks {
+    [key: number]: ContractData;
+};
+
+export interface ContractData {
+    solc_version: string;
+    optimizer_runs: number;
+    keccak256: string;
+    abi: Web3.ContractAbi;
+    unlinked_binary: string;
+    address?: string;
+    updated_at: number;
+};
+
+export interface SolcErrors {
+    [key: string]: boolean;
+};
+
+export interface CompilerOptions {
+    contractsDir: string;
+    networkId: number;
+    optimizerRuns: number;
+    artifactsDir: string;
+};
+
+export interface ContractSources {
+    [key: string]: string;
+};
+
+export interface ImportContents {
+    contents: string;
+};
