c3b45e9bc484e17c3aa2c4f5dc9abfadd7cc005e
Fabio Berger <me@fabioberger.com>
2017-10-04 10:56:54 +0300

    remove unnecessary async wrapper

--- scripts/generate_doc_json.ts
+++ scripts/generate_doc_json.ts
@@ -6,31 +6,28 @@ import * as child from 'child_process';
 const tempDir = path.resolve('temporary');
 const DOC_JSONS_DIR = `${tempDir}/doxity/pages/docs`;
 
-(async () => {
+const stdout = child.execSync('git tag');
+const version = stdout.toString('utf8');
+const FINAL_DOC_FILE_PATH = `${DOC_JSONS_DIR}/v${version}.json`;
 
-    const stdout = child.execSync('git tag');
-    const version = stdout.toString('utf8');
-    const FINAL_DOC_FILE_PATH = `${DOC_JSONS_DIR}/v${version}.json`;
+// Remove finalDoc json if exists
+if (fs.existsSync(FINAL_DOC_FILE_PATH)) {
+    fs.unlinkSync(FINAL_DOC_FILE_PATH);
+}
 
-    // Remove finalDoc json if exists
-    if (fs.existsSync(FINAL_DOC_FILE_PATH)) {
-        fs.unlinkSync(FINAL_DOC_FILE_PATH);
-    }
+const finalDocJSON: {[contractName: string]: any} = {};
 
-    const finalDocJSON: {[contractName: string]: any} = {};
+// For each .json file in dir, add it with name as key to final JSON
+const fileNames = fs.readdirSync(DOC_JSONS_DIR);
+const jsonFiles = _.filter(fileNames, fileName => _.includes(fileName, '.json'));
 
-    // For each .json file in dir, add it with name as key to final JSON
-    const fileNames = fs.readdirSync(DOC_JSONS_DIR);
-    const jsonFiles = _.filter(fileNames, fileName => _.includes(fileName, '.json'));
+for (const jsonFile of jsonFiles) {
+    const contentString = fs.readFileSync(`${DOC_JSONS_DIR}/${jsonFile}`).toString();
+    const content = JSON.parse(contentString);
+    delete content.source; // Remove source
+    finalDocJSON[content.name] = content;
+}
 
-    for (const jsonFile of jsonFiles) {
-        const contentString = fs.readFileSync(`${DOC_JSONS_DIR}/${jsonFile}`).toString();
-        const content = JSON.parse(contentString);
-        delete content.source; // Remove source
-        finalDocJSON[content.name] = content;
-    }
-
-    const finalDocString = JSON.stringify(finalDocJSON);
-    fs.writeFileSync(FINAL_DOC_FILE_PATH, finalDocString);
-    console.log('Successfully generated Doc JSON');
-})();
+const finalDocString = JSON.stringify(finalDocJSON);
+fs.writeFileSync(FINAL_DOC_FILE_PATH, finalDocString);
+console.log('Successfully generated Doc JSON');
