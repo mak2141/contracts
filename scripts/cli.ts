4d544fcde8a7c80fba6e03891190af77a848d6e1
Amir Bandeali <abandeali1@gmail.com>
2017-11-09 13:51:18 -0500

    Add compile command to cli

--- scripts/cli.ts
+++ scripts/cli.ts
@@ -7,39 +7,47 @@ const DEFAULT_OPTIMIZER_ENABLED = false;
 const DEFAULT_CONTRACTS_DIR = path.resolve('contracts');
 const DEFAULT_ARTIFACTS_DIR = `${path.resolve('build')}/artifacts/`;
 const DEFAULT_NETWORK_ID = 50;
+const DEFAULT_JSONRPC_PORT = 8545;
 
-(async () => {
-    const args = yargs
-        .option('contracts-dir', {
-            type: 'string',
-            default: DEFAULT_CONTRACTS_DIR,
-            description: 'path of contracts directory to compile',
-        })
-        .option('network-id', {
-            type: 'number',
-            default: DEFAULT_NETWORK_ID,
-            description: 'mainnet=1, kovan=42, testrpc=50',
-        })
-        .option('optimize', {
-            type: 'boolean',
-            default: DEFAULT_OPTIMIZER_ENABLED,
-            description: 'enable optimizer',
-        })
-        .option('artifacts-dir', {
-            type: 'string',
-            default: DEFAULT_ARTIFACTS_DIR,
-            description: 'path to write contracts artifacts to',
-        })
-        .help()
-        .argv;
+const args = yargs
+    .option('contracts-dir', {
+        type: 'string',
+        default: DEFAULT_CONTRACTS_DIR,
+        description: 'path of contracts directory to compile',
+    })
+    .option('network-id', {
+        type: 'number',
+        default: DEFAULT_NETWORK_ID,
+        description: 'mainnet=1, kovan=42, testrpc=50',
+    })
+    .option('optimize', {
+        type: 'boolean',
+        default: DEFAULT_OPTIMIZER_ENABLED,
+        description: 'enable optimizer',
+    })
+    .option('artifacts-dir', {
+        type: 'string',
+        default: DEFAULT_ARTIFACTS_DIR,
+        description: 'path to write contracts artifacts to',
+    })
+    .option('jsonrpc-port', {
+        type: 'number',
+        default: DEFAULT_JSONRPC_PORT,
+        description: 'port connected to JSON RPC',
+    })
+    .help()
+    .argv;
 
-    const options: CompilerOptions = {
-        contractsDir: args.contractsDir,
-        networkId: args.networkId,
-        optimizerEnabled: args.optimize ? 1: 0,
-        artifactsDir: args.artifactsDir,
-    };
+yargs
+    .command('compile', 'compile contracts', () => {}, async (): Promise<void> => {
+        const options: CompilerOptions = {
+            contractsDir: args.contractsDir,
+            networkId: args.networkId,
+            optimizerEnabled: args.optimize ? 1 : 0,
+            artifactsDir: args.artifactsDir,
+        };
 
-    const compiler = new Compiler(options);
-    await compiler.compileAllAsync();
-})();
+        const compiler = new Compiler(options);
+        await compiler.compileAllAsync();
+    })
+    .argv;
